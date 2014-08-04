2f09eaaa9e2acc5ab8b36b1373914afd3885262a
Gabriel Rodríguez Alberich <gabi@gabi.is>
2014-08-04 12:32:39 +0200

    Adapted to new TED site.

--- ted-scrape.py
+++ ted-scrape.py
@@ -18,6 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
+import argparse
 import os
 import sys
 import re
@@ -28,22 +29,21 @@ import urllib2
 
 
 _USERAGENT = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.0.5)' \
-            ' Gecko/2008121622 Ubuntu/8.04 (hardy) Firefox/3.0.5'
+             ' Gecko/2008121622 Ubuntu/8.04 (hardy) Firefox/3.0.5'
 headers = {'User-Agent': _USERAGENT}
 
 last_url = "http://feeds.feedburner.com/tedtalks_video"
 view_url_p = "http://ted.com/talks/view/id/%(video_id)s"
-video_re = 'href="http://download.ted.com/talks/(?P<video_name>.+?).mp4\?' \
-           'apikey=TEDDOWNLOAD">download the video</a> to play.'
+video_re = '<script>q\("talkPage.init",(?P<video_json>.*?)\)<\/script>'
 author_re = '<a target="_blank" title="(?P<author_name>.+?)\'s bio" ' \
             'href="/speakers/.*?\.html">Full bio'
 date_re = '<strong>Filmed</strong> (?P<filmed_date>.+?) &bull;'
 event_re = '<span class="event-name">(?P<event>.+?)</span>'
 
 
-def main():
-    last_talks = urllib2.urlopen(urllib2.Request(last_url, headers=headers)). \
-                 read()
+def main(overwrite=False):
+    last_talks = urllib2.urlopen(urllib2.Request(last_url, headers=headers)).\
+                    read()
     root = etree.fromstring(last_talks)
     candidate_ids = [int(x.text) for x in root.xpath("//a:talkId",
                      namespaces={'a': 'http://developer.longtailvideo.com/'})]
@@ -51,6 +51,9 @@ def main():
     if os.path.isfile("ted-scrape.json"):
         with open("ted-scrape.json", "r") as f:
             ted_videos = json.load(f)
+            if overwrite:
+                 ted_videos = [x for x in ted_videos if x['video_id'] not in
+                               candidate_ids]
             video_ids = [x['video_id'] for x in ted_videos]
             # Note: this will work as expected only if the script is executed
             # frequently enough as to not let the RSS feed fill with new talks
@@ -85,33 +88,20 @@ def main():
         if not video_page:
             continue
 
-        match = re.search(video_re, video_page)
-        if not match:
-            print("Couldn't parse video page for download URL. Skipping.",
+        try:
+            match = re.search(video_re, video_page)
+            video_json = json.loads(match.groupdict()['video_json'])
+            video_dict = video_json['talks'][0]
+        except AttributeError:
+            print("Couldn't parse video page for JSON. Skipping.",
                   file=sys.stderr)
             continue
 
-        video_dict.update(match.groupdict())
+        video_dict['video_id'] = video_id
 
         tree = html.fromstring(video_page)
 
-        lang_options = tree.xpath("//select[@id='languageCode']/option")
-        if lang_options:
-            lang_codes = filter(None, [option.attrib.get('value', None)
-                                for option in lang_options])
-            video_dict['lang_codes'] = lang_codes
-        else:
-            print("Couldn't parse video page for language codes.",
-                  file=sys.stderr)
-
-        title_span = tree.xpath("//span[@id='altHeadline']")
-        if title_span:
-            title = title_span[0].text
-            video_dict['title'] = title
-        else:
-            print("Couldn't parse video page for title.", file=sys.stderr)
-
-        summary_p = tree.xpath("//p[@id='tagline']")
+        summary_p = tree.xpath("//p[@class='talk-description']")
         if summary_p:
             summary = etree.tostring(summary_p[0], encoding=unicode,
                                      method="text").strip()
@@ -119,48 +109,34 @@ def main():
         else:
             print("Couldn't parse video page for summary.", file=sys.stderr)
 
-        author_match = re.search(author_re, video_page)
-        if author_match:
-            video_dict['author'] = author_match.groupdict()['author_name']
-        else:
-            print("Couldn't parse video page for author.", file=sys.stderr)
-
-        tags_a = tree.xpath("//dd[@class='clearfix tags notranslate']/ul/li/a")
+        tags_a = tree.xpath("//ul[@class='talk-topics__list']//a")
         if tags_a:
-            tags = [tag.text for tag in tags_a]
+            tags = [tag.text.strip() for tag in tags_a]
             video_dict['tags'] = tags
         else:
             print("Couldn't parse video page for tags.", file=sys.stderr)
 
-        date_match = re.search(date_re, video_page)
-        if date_match:
-            date_string = date_match.groupdict()['filmed_date']
-            video_date = datetime.datetime.strptime(date_string, '%b %Y')
-            video_dict['filmed_date'] = video_date.isoformat()
-        else:
-            print("Couldn't parse video page for the filmed date.",
-                  file=sys.stderr)
-
-        event_match = re.search(event_re, video_page)
-        if event_match:
-            video_dict['event'] = event_match.groupdict()['event']
-        else:
-            print("Couldn't parse video page for the event.", file=sys.stderr)
-
-        video_script = tree.xpath("//div[@class='leftColumn']/script")
-        if video_script:
-            script_text = urllib2.unquote(video_script[0].text)
-            intro_match = re.search('"introDuration":(?P<intro>.*?),',
-                                    script_text)
-            if intro_match:
-                intro = intro_match.groupdict()['intro']
-                video_dict['intro_duration'] = intro
-
         ted_videos.append(video_dict)
 
     with open("ted-scrape.json", "w") as f:
-        json.dump(ted_videos, f)
+        json.dump(ted_videos, f, indent=2)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='tedtalks.eu downloader'
+    )
+    parser.add_argument(
+        '-o', '--overwrite',
+        action='store_true',
+        help='overwrite previously downloaded talks (only affects talks ' \
+             'present in the RSS feed)'
+    )
+
+    args = parser.parse_args()
+    if args.overwrite:
+        main(overwrite=True)
+    else:
+        main()
+
